3ea450bdc5d41e2b7bd0994231edb184bf37efa7
Michael Sundqvist <Alhetus@users.noreply.github.com>
2021-10-15 14:39:42 +0300

    Add ApiClient module to handle all requests to Tridify API (#11)
    * Add ApiClient module to handle all requests to Tridify API
    
    * Fix property names on SharedconversionFileDTO

---
+++ src/DTO/DTO.ts
@@ -0,0 +1,47 @@
+export interface PublishedLinkDTO {
+  Conversions: SharedConversionDTO[];
+  Configuration: SharedConfigurationDTO;
+  LinkEnabled: boolean;
+  PostProcessState: string; // TODO: Change to enum
+  PostProcessedFiles: string[];
+  LinkedFiles: LinkedFileDTO[];
+}
+
+export interface SharedConversionDTO {
+  Hash: string;
+  Files: SharedConversionFileDTO[];
+  FileName: string;
+}
+
+export interface SharedConversionFileDTO {
+  Url: string;
+  Type: string;
+  Format: string;
+  Storey: string;
+  Overlay: boolean;
+  Guid: string;
+  FileName: string | undefined; // undefined for old conversions
+}
+
+export interface SharedConfigurationDTO {
+  Tools: ToolsDTO;
+  PropertySetNames: string[];
+  QuantityNames: string[];
+}
+
+export interface ToolsDTO {
+  VRHeadsetMode: boolean;
+  ShareViewer: boolean;
+  MeasureTool: boolean;
+  BimTool: boolean;
+  CuttingPlanesTool: boolean;
+  WaypointTool: boolean;
+  CombinationVisibilityTool: boolean;
+  CommentingTool: boolean;
+  LocationTool: boolean;
+}
+
+export interface LinkedFileDTO {
+  OriginalFileName: string;
+  Url: string;
+}
