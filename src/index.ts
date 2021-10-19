b204dc7d036ea9831faaaf9bfb1bff2b29c79d84
Muusssari <muussari@hotmail.com>
2021-10-19 03:27:31 +0300

    update tslib, add frame scene

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ export * as ApiClient from "./ApiClient";
 export * as GltfLoader from "./GltfLoader";
 export * as AbstractMeshExtensions from "./Extensions/AbstractMeshExtensions";
 export * as MeshExtensions from "./Extensions/MeshExtensions";
+export * as Utilities from "./Utilities";
